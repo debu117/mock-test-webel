b676790a1fcbec65aa7289697e9dbc710c8fc86e
DEBOJYOTI BOSE <96744623+debu117@users.noreply.github.com>
2026-06-29 16:43:30 +0530

    Update CORS middleware to allow all origins
    Simplified CORS middleware configuration to allow all origins.

--- backend/server.js
+++ backend/server.js
@@ -13,15 +13,7 @@ const PORT = process.env.PORT || 8080;
 
 //MIDDLEWARES
 
-app.use(
-    cors({
-        origin: [
-            "http://localhost:5173",
-            "http://localhost:5174"
-        ],
-        credentials: true,
-    })
-); //as the user will be found first then we use the JSON.
+app.use(cors()); //as the user will be found first then we use the JSON.
 app.use(express.json());
 
 app.use(clerkMiddleware());
