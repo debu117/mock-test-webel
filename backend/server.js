cfd569b4ee2ed32c3b58557d944e94c5373db949
Debojyoti <debojyotibose001@gmail.com>
2026-06-29 16:40:47 +0530

    Updated API URL for production

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import dns from "dns";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from '@clerk/express';
 import { connectDB } from './config/db.js';
 import userRoutes from './routes/user.js';
 import adminRoutes from './routes/admin.js';
@@ -10,37 +10,32 @@ import resultRoutes from './routes/result.js';
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-//MIDDLEWARES
-
+// MIDDLEWARES
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://mock-test-webel.vercel.app",      // Admin
-      "https://mock-test-webel-2vta.vercel.app"  // Student frontend
+      "https://mock-test-webel.vercel.app",
+      "https://mock-test-webel-2vta.vercel.app"
     ],
     credentials: true,
   })
 );
 app.use(express.json());
-
 app.use(clerkMiddleware());
 app.use("/api/users", userRoutes);
-app.use("/api/admin", adminRoutes)
+app.use("/api/admin", adminRoutes);
 app.use("/api/result", resultRoutes);
 
-//DB
+// DB
 connectDB();
 
-//ROUTES
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
 
-
-app.get("/", (req,res)=>{
-    res.send("API WORKING");
-})
-
-app.listen(PORT, () =>{
-    console.log(`Server Started on http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server Started on http://localhost:${PORT}`);
+});
